43e1c2b84af0a490acaf693c86b807c062a3f184
Graeme Harrison <gharrison365@gmail.com>
2016-02-04 12:15:45 -0500

    First commit

---
+++ Receipts++/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Receipts++
+//
+//  Created by Graeme Harrison on 2016-02-04.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

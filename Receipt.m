0a60a9c063253c94a5f2d4a3e9edccc6a28024d1
Graeme Harrison <gharrison365@gmail.com>
2016-02-04 12:37:04 -0500

    Added Receipt and Tag entities

---
+++ Receipt.m
@@ -0,0 +1,15 @@
+//
+//  Receipt.m
+//  Receipts++
+//
+//  Created by Graeme Harrison on 2016-02-04.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import "Receipt.h"
+
+@implementation Receipt
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
---
+++ Tag.m
@@ -0,0 +1,16 @@
+//
+//  Tag.m
+//  Receipts++
+//
+//  Created by Graeme Harrison on 2016-02-04.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import "Tag.h"
+#import "Receipt.h"
+
+@implementation Tag
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
